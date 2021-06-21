78fecb35c881d5a641bb2dbc795964feafbf7a96
Abhijeet Kumar Gupta <84043354+AbhijeetKumarGupta@users.noreply.github.com>
2021-06-21 18:06:20 +0530

    Add files via upload

---
+++ J. Quiz.py
@@ -0,0 +1,521 @@
+import random
+import sys
+import os
+
+print('**********************************')
+print('* Chatting room - Reply Cleverly *')
+print('**********************************\n')
+print('  ***WELCOME TO JAPANESE TEST***\n')
+print('**********************************\n')
+print('1. こんにちは!!\n')
+print('**********************************\n')
+print('\n')
+print('Press 1 - To convert it in ENGLISH \nPress 2 - To Reply')
+a=int(input())
+if a == 1:
+    print('\n**********************************\n')
+    print('1. HELLO!! --- KONICHIWA!!\n')
+    print('**********************************\n')
+    b = 5
+    while b>4: 
+        print('\n')
+        c='KONICHIWA'
+        d='HELLO'
+        e='HI'
+        f='HEY'
+        g='NAMASTE'
+        h='HOLA'
+        i='SALAAM'
+        j='HELO'
+        b=input()
+        if b==c:
+            print('\n**********************************\n')
+            print('Sugoi\n')
+            print('**********************************\n')
+            b = 1
+        elif b==g:
+            print('\n**********************************\n')
+            print('Acha hai!\n')
+            print('**********************************\n')
+            b = 1
+        elif b==d:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==h:
+            print('\n**********************************\n')
+            print('Derecho\n')
+            print('**********************************\n')
+            b = 2
+        elif b==e:
+            print('\n**********************************\n')
+            print('Great\n')
+            print('**********************************\n')
+            b = 3
+        elif b==i:
+            print('\n**********************************\n')
+            print('Great\n')
+            print('**********************************\n')
+            b = 3
+        elif b==f:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        elif b==j:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        else:
+            print('\nTry again\n')
+            b = 5
+else:
+    b = 5
+    while b>4: 
+        print('\n')
+        c='KONICHIWA'
+        d='HELLO'
+        e='HI'
+        f='HEY'
+        g='NAMASTE'
+        h='HOLA'
+        i='SALAAM'
+        j='HELO'
+        b=input()
+        if b==c:
+            print('\n**********************************\n')
+            print('Sugoi\n')
+            print('**********************************\n')
+            b = 1
+        elif b==g:
+            print('\n**********************************\n')
+            print('Acha hai!\n')
+            print('**********************************\n')
+            b = 1
+        elif b==d:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==h:
+            print('\n**********************************\n')
+            print('Derecho\n')
+            print('**********************************\n')
+            b = 2
+        elif b==e:
+            print('\n**********************************\n')
+            print('Great\n')
+            print('**********************************\n')
+            b = 3
+        elif b==i:
+            print('\n**********************************\n')
+            print('Great\n')
+            print('**********************************\n')
+            b = 3
+        elif b==f:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        elif b==j:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        else:
+            print('\nTry again\n')
+            b = 5
+
+print('**********************************\n')
+print('2. おげんきですか?\n')
+print('**********************************\n')
+print('\n')
+print('Press 1 - To convert it in ENGLISH \nPress 2 - To Reply')
+a=int(input())
+if a == 1:
+    print('\n**********************************\n')
+    print('2. How are you? --- Ogenkidesuka? \n')
+    print('**********************************\n')
+    b = 5
+    while b>4: 
+        print('\n')
+        c='GENKIDESU'
+        d='ESTOY BIEN'
+        e='I AM FINE'
+        f='NOT SO GOOD'
+        g='GOOD'
+        h='AWESOME'
+        i='VERY BAD'
+        j='OK'
+        b=input()
+        if b==c:
+            print('\n**********************************\n')
+            print('Sugoi\n')
+            print('**********************************\n')
+            b = 1
+        elif b==g:
+            print('\n**********************************\n')
+            print('Derecho\n')
+            print('**********************************\n')
+            b = 1
+        elif b==d:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==h:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==e:
+            print('\n**********************************\n')
+            print('Great\n')
+            print('**********************************\n')
+            b = 3
+        elif b==i:
+            print('\n**********************************\n')
+            print('Oh!! Sorry\n')
+            print('**********************************\n')
+            b = 3
+        elif b==f:
+            print('\n**********************************\n')
+            print('God Bless!\n')
+            print('**********************************\n')
+            b = 4
+        elif b==j:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        else:
+            print('\nTry again\n')
+            b = 5
+else:
+    b = 5
+    while b>4: 
+        print('\n')
+        c='GENKIDESU'
+        d='ESTOY BIEN'
+        e='I AM FINE'
+        f='NOT SO GOOD'
+        g='GOOD'
+        h='AWESOME'
+        i='VERY BAD'
+        j='OK'
+        b=input()
+        if b==c:
+            print('\n**********************************\n')
+            print('Sugoi\n')
+            print('**********************************\n')
+            b = 1
+        elif b==g:
+            print('\n**********************************\n')
+            print('Derecho\n')
+            print('**********************************\n')
+            b = 1
+        elif b==d:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==h:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==e:
+            print('\n**********************************\n')
+            print('Great\n')
+            print('**********************************\n')
+            b = 3
+        elif b==i:
+            print('\n**********************************\n')
+            print('Oh!! Sorry\n')
+            print('**********************************\n')
+            b = 3
+        elif b==f:
+            print('\n**********************************\n')
+            print('God Bless!\n')
+            print('**********************************\n')
+            b = 4
+        elif b==j:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        else:
+            print('\nTry again\n')
+            b = 5
+
+print('**********************************\n')
+print('3. おなまえわなんですか?\n')
+print('**********************************\n')
+print('\n')
+print('Press 1 - To convert it in ENGLISH \nPress 2 - To Reply')
+a=int(input())
+if a == 1:
+    print('\n**********************************************\n')
+    print('3. What is your name? --- Onamaewanandesuka?\n')
+    print('**********************************************\n')
+    print('\n')
+    b = str(input())
+    print('\n**********************************\n')
+    print('Hey!! '+b)
+    print('My name is Abhijeet Gupta\n')
+    print('**********************************\n')
+else:
+   print('\n')
+   b = str(input())
+   print('\n*************************************\n')
+   print('\nKonichiwa!! '+b)
+   print('Watashi no namae wa Abhijeet Gupta desu\n')
+   print('*************************************\n')
+            
+print('**********************************\n')
+print('4. はじめまして!\n')
+print('**********************************\n')
+print('\n')
+print('Press 1 - To convert it in ENGLISH \nPress 2 - To Reply')
+a=int(input())
+if a == 1:
+    print('\n***************************************\n')
+    print('4. Nice to meet you! --- Hajimemashite!\n')
+    print('***************************************\n')
+    b = 5
+    while b>4: 
+        print('\n')
+        c='HAJIMEMASHITE'
+        d='NICE TO MEET YOU'
+        e='MUCHO GUSTO'
+        f='SAME HERE'
+        g='NICE TO MEET YOU TOO'
+        h='ME 2'
+        i='HERE ALSO'
+        j='MY PLEASURE'
+        b=input()
+        if b==c:
+            print('\n**********************************\n')
+            print('Sugoi\n')
+            print('**********************************\n')
+            b = 1
+        elif b==g:
+            print('\n**********************************\n')
+            print('Right\n')
+            print('**********************************\n')
+            b = 1
+        elif b==d:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==h:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==e:
+            print('\n**********************************\n')
+            print('Derecho\n')
+            print('**********************************\n')
+            b = 3
+        elif b==i:
+            print('\n**********************************\n')
+            print('Great\n')
+            print('**********************************\n')
+            b = 3
+        elif b==f:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        elif b==j:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        else:
+            print('\nTry again\n')
+            b = 5
+else:
+    b = 5
+    while b>4: 
+        print('\n')
+        c='HAJIMEMASHITE'
+        d='NICE TO MEET YOU'
+        e='MUCHO GUSTO'
+        f='SAME HERE'
+        g='NICE TO MEET YOU TOO'
+        h='ME 2'
+        i='HERE ALSO'
+        j='MY PLEASURE'
+        b=input()
+        if b==c:
+            print('\n**********************************\n')
+            print('Sugoi\n')
+            print('**********************************\n')
+            b = 1
+        elif b==g:
+            print('\n**********************************\n')
+            print('Right\n')
+            print('**********************************\n')
+            b = 1
+        elif b==d:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==h:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==e:
+            print('\n**********************************\n')
+            print('Derecho\n')
+            print('**********************************\n')
+            b = 3
+        elif b==i:
+            print('\n**********************************\n')
+            print('Great\n')
+            print('**********************************\n')
+            b = 3
+        elif b==f:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        elif b==j:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        else:
+            print('\nTry again\n')
+            b = 5
+
+print('**********************************\n')
+print('5. さようなら, じゃあまたね!\n')
+print('**********************************\n')
+print('\n')
+print('Press 1 - To convert it in ENGLISH \nPress 2 - To Reply')
+a=int(input())
+if a == 1:
+    print('\n***********************************************\n')
+    print('5. Bye, See you later! --- Sayonara, Jaamatane\n')
+    print('***********************************************\n')
+    b = 5
+    while b>4: 
+        print('\n')
+        c='SAYONARA'
+        d='SEE YOU LATER'
+        e='ADIOS'
+        f='LATER'
+        g='BYE, SEE YOU LATER'
+        h='ADIOS, HASTA LUEGO'
+        i='HASTA MANYANA'
+        j='SAYONARA, JAAMATANE'
+        b=input()
+        if b==c:
+            print('\n**********************************\n')
+            print('Sugoi\n')
+            print('**********************************\n')
+            b = 1
+        elif b==g:
+            print('\n**********************************\n')
+            print('Right\n')
+            print('**********************************\n')
+            b = 1
+        elif b==d:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==h:
+            print('\n**********************************\n')
+            print('Derecho\n')
+            print('**********************************\n')
+            b = 2
+        elif b==e:
+            print('\n**********************************\n')
+            print('Derecho\n')
+            print('**********************************\n')
+            b = 3
+        elif b==i:
+            print('\n**********************************\n')
+            print('Great\n')
+            print('**********************************\n')
+            b = 3
+        elif b==f:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        elif b==j:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        else:
+            print('\nTry again\n')
+            b = 5
+else:
+    b = 5
+    while b>4: 
+        print('\n')
+        c='SAYONARA'
+        d='SEE YOU LATER'
+        e='ADIOS'
+        f='LATER'
+        g='BYE, SEE YOU LATER'
+        h='ADIOS, HASTA LUEGO'
+        i='HASTA MANYANA'
+        j='SAYONARA, JAAMATANE'
+        b=input()
+        if b==c:
+            print('\n**********************************\n')
+            print('Sugoi\n')
+            print('**********************************\n')
+            b = 1
+        elif b==g:
+            print('\n**********************************\n')
+            print('Right\n')
+            print('**********************************\n')
+            b = 1
+        elif b==d:
+            print('\n**********************************\n')
+            print('Good\n')
+            print('**********************************\n')
+            b = 2
+        elif b==h:
+            print('\n**********************************\n')
+            print('Derecho\n')
+            print('**********************************\n')
+            b = 2
+        elif b==e:
+            print('\n**********************************\n')
+            print('Derecho\n')
+            print('**********************************\n')
+            b = 3
+        elif b==i:
+            print('\n**********************************\n')
+            print('Great\n')
+            print('**********************************\n')
+            b = 3
+        elif b==f:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        elif b==j:
+            print('\n**********************************\n')
+            print('Yup\n')
+            print('**********************************\n')
+            b = 4
+        else:
+            print('\nTry again\n')
+            b = 5
+
+z = input()
